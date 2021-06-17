042148beaf58b6b6d738404752ae2881b26fcdf5
Rose Barcus <angela.rose.barcus@gmail.com>
2021-06-17 15:46:28 -0500

    completed tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,11 @@
 USE codeup_test_db;
 
-DROP TABLE IF EXISTS albums
-
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
+    artist VARCHAR(50),
     name VARCHAR(100) NOT NULL,
-    release_date smallint DEFAULT 'UNKNOWN',
-    sales float,
-    genre VARCHAR(50) DEFAULT 'NONE',
-    PRIMARY KEY (id)
+    release_date DATE,
+    sales FLOAT,
+    genre VARCHAR(50),
+    PRIMARY KEY(id)
 );
