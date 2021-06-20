6933beb914288a3d4e14f2f27fb848ab93a1ac22
Rose Barcus <angela.rose.barcus@gmail.com>
2021-06-20 12:06:16 -0500

    corrected where clause exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,26 +1,39 @@
+# Part One
+
 USE employees;
 
-SELECT first_name, gender
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+# Part Two
+
+SELECT * FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+SELECT * FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
   AND gender = 'M';
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%';
 
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%e';
 
-SELECT *
-FROM employees
-WHERE last_name LIKE '%e'
-   OR last_name like 'e%';
+SELECT * FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e';
 
-SELECT *
-FROM employees
-WHERE last_name LIKE '%e'
-   AND last_name like 'e%';
+# or you can do this:
 
-SELECT * FROM employees WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+SELECT * FROM employees
+WHERE last_name LIKE 'E%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
