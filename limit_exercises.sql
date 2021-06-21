b32dc6e304175f4e94e084b237fdbac15053a10d
Rose Barcus <angela.rose.barcus@gmail.com>
2021-06-21 15:18:10 -0500

    completed LIMIT and OFFSET exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+
+SElECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC limit 10;
+
+SELECT DISTINCT emp_no FROM salaries
+ORDER BY salary DESC LIMIT 5;
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
+
+
+
