c19748b918544def09e7dd1d2a7b189b1ec18605
Rose Barcus <angela.rose.barcus@gmail.com>
2021-06-22 09:30:20 -0500

    completed functions exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -43,6 +43,12 @@ SELECT * FROM employees
 WHERE last_name LIKE '%q%'
   AND last_name NOT LIKE '%qu%';
 
+#Functions exercises
+
+SELECT CONCAT(first_name, ' ', last_name) FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e';
+
 #All employees born on Christmas:
 
 SELECT *
@@ -56,4 +62,19 @@ WHERE YEAR (hire_date) BETWEEN 1990 AND 2000
 AND MONTH (birth_date) = 12
 AND DAY (birth_date) = 25
 ORDER BY birth_date DESC;
+
+# SELECT * FROM employees
+# WHERE DAY (birth_date) = 25
+#   AND MONTH (birth_date) = 25
+#   AND YEAR (hire_date) = LIKE '199%'
+#   ORDER BY birth_date, hire_date DESC;
+
+SELECT *, DATEDIFF(hire_date, CURDATE()) FROM employees
+WHERE DAY (birth_date) = 25
+AND MONTH (birth_date) = 12
+AND YEAR (hire_date) LIKE '199%'
+ORDER BY DATEDIFF(hire_date, CURDATE());
+
+
+
 
