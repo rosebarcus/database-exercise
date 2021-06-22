51eb18260876db85f9553f6325959b713324a478
Rose Barcus <angela.rose.barcus@gmail.com>
2021-06-22 10:46:51 -0500

    completed group by exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,20 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name LIKE 'E%e'
+ORDER BY last_name;
+
+SELECT DISTINCT first_name, last_name FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY last_name,first_name;
+
+SELECT DISTINCT last_name, COUNT(*) FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT gender, COUNT(*) FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
